591a7fc29613e0865c46be43edbe35948067b3e1
taul8123 <51949055+taul8123@users.noreply.github.com>
2023-05-28 16:01:12 +0900

    2.1.2 About Us Bug fix

--- public/javascript/about.js
+++ public/javascript/about.js
@@ -1,17 +1,14 @@
 $(document).ready(function(){
-    $("#image-frame1").hover(function(){
-        $("#image-caption1").toggle("slow");
-    }, function(){
-        $("#image-caption1").toggle("slow")
-    });
-    $("#image-frame2").hover(function(){
-        $("#image-caption2").toggle("slow");
-    }, function (){
-        $("#image-caption2").toggle("slow");
-    });
-    $("#image-frame3").hover(function(){
-        $("#image-caption3").toggle("slow");
-    }, function (){
-        $("#image-caption3").toggle("slow");
-    });
+  $("#image-frame1").on({
+    mouseenter: function(){
+        $("#image-caption1").toggle("slow");}
+  });
+  $("#image-frame2").on({
+    mouseenter: function(){
+        $("#image-caption2").toggle("slow");}
+  });
+  $("#image-frame3").on({
+    mouseenter: function(){
+        $("#image-caption3").toggle("slow");}
+  });
 });
