a81907087448bf778a977f3768e807cc26c62854
taul8123 <51949055+taul8123@users.noreply.github.com>
2023-05-29 14:34:44 +0900

    KillTOM 2.1

--- src/change_view.js
+++ src/change_view.js
@@ -2,38 +2,33 @@ let home = document.getElementById("home");
 let web_console = document.getElementById("web_console");
 let reference = document.getElementById("reference");
 let about = document.getElementById("about");
+
 const change_view = {
-    show_home : show_home(),
-    show_console : show_console(),
-    show_reference : show_reference(),
-    show_about : show_about()
-}
-function show_home() {
-    home.style.display = "flex";
-    web_console.style.display = "none";
-    reference.style.display = "none";
-    about.style.display = "none";
-}
-
-function show_console() {
-    home.style.display = "none";
-    web_console.style.display = "flex";
-    reference.style.display = "none";
-    about.style.display = "none";
-}
-
-function show_reference() {
-    home.style.display = "none";
-    web_console.style.display = "none";
-    reference.style.display = "flex";
-    about.style.display = "none";
-}
-
-function show_about() {
-    home.style.display = "none";
-    web_console.style.display = "none";
-    reference.style.display = "none";
-    about.style.display = "block";
+    show_home : function show_home() {
+        home.style.display = "flex";
+        web_console.style.display = "none";
+        reference.style.display = "none";
+        about.style.display = "none";
+    },
+    show_console : function show_console() {
+        home.style.display = "none";
+        web_console.style.display = "flex";
+        reference.style.display = "none";
+        about.style.display = "none";
+    },
+    show_reference : function show_reference() {
+        home.style.display = "none";
+        web_console.style.display = "none";
+        reference.style.display = "flex";
+        about.style.display = "none";
+    },
+    show_about : function show_about() {
+        home.style.display = "none";
+        web_console.style.display = "none";
+        reference.style.display = "none";
+        about.style.display = "block";
+    }
+    
 }
 
 export default change_view;
