de7b041e4db10a467cfe9b343ca2caee2ba81349
JEON-TOM <101195937+jjh6593@users.noreply.github.com>
2023-05-30 18:36:44 +0900

    2.1.3.1 Change map info

--- public/javascript/map_script.js
+++ public/javascript/map_script.js
@@ -11,22 +11,14 @@ function initMap() {
 	});
 	
 	
-	const contentString = "<div style='width:150px;'>" +
+	const contentString = ["<div style='padding:10px;'>" +
 	"<div style='float:left;'>" +
-	"<a href='https://map.kakao.com/?urlX=601621.0&urlY=868907.0&itemId=1236538181&q=%EC%B6%A9%EB%B6%81%EB%8C%80%ED%95%99%EA%B5%90%20%EC%A0%84%EC%9E%90%EC%A0%95%EB%B3%B4%EB%8C%80%ED%95%993%EA%B4%80&srcid=1236538181&map_type=TYPE_MAP&from=roughmap' target='_blank'>충북대학교 소프트웨어학과</a></div></div>";
-	
+	"<h4>충북대학교 S4-1</h4>"+
+	"<a href='https://map.kakao.com/?urlX=601621.0&urlY=868907.0&itemId=1236538181&q=%EC%B6%A9%EB%B6%81%EB%8C%80%ED%95%99%EA%B5%90%20%EC%A0%84%EC%9E%90%EC%A0%95%EB%B3%B4%EB%8C%80%ED%95%993%EA%B4%80&srcid=1236538181&map_type=TYPE_MAP&from=roughmap' target='_blank'>길찾기</a></div></div>"
+].join('');
 	
 	const infowindow = new naver.maps.InfoWindow({
 		content: contentString,
-		backgroundColor: "#fff",   // 말풍선 컬러를 흰색으로 변경합니다.
-		borderColor: "#ccc",       // 말풍선 테두리 색상을 변경합니다.
-		borderWidth: 1,            // 말풍선 테두리 두께를 1px으로 변경합니다.
-		
-		anchorSize: new naver.maps.Size(0, 0),
-		anchorSkew: true,
-		pixelOffset: new naver.maps.Point(0, -10),
-		disableAnchor: true
-		
 	});
 	
 	naver.maps.Event.addListener(marker, "click", function () {
