7937f1cadc0432469974d568dc09e739c77cffd8
hyun-sa <65175154+hyun-sa@users.noreply.github.com>
2023-06-08 16:54:43 +0900

    2.4.2 Add Animation
    absdfasvdfcdfv

--- public/javascript/change_view.js
+++ public/javascript/change_view.js
@@ -4,32 +4,42 @@ let reference = document.getElementById("reference");
 let about = document.getElementById("about");
 function show_home() {
     home.style.display = "flex";
+    home.classList.add("fade-in");
     web_console.style.display = "none";
     reference.style.display = "none";
+    reference.classList.remove("fade-in");
     about.style.display = "none";
-   
+    about.classList.remove("fade-in");
 }
 
 function show_console() {
     home.style.display = "none";
+    home.classList.remove("fade-in");
     web_console.style.display = "flex";
     reference.style.display = "none";
+    reference.classList.remove("fade-in");
     about.style.display = "none";
-   
+    about.classList.remove("fade-in");
 }
 
 function show_reference() {
     home.style.display = "none";
+    home.classList.remove("fade-in");
     web_console.style.display = "none";
     reference.style.display = "flex";
+    reference.classList.add("fade-in");
     about.style.display = "none";
-
+    about.classList.remove("fade-in");
 }
 
 function show_about() {
     home.style.display = "none";
+    home.classList.remove("fade-in");
     web_console.style.display = "none";
     reference.style.display = "none";
+    reference.classList.remove("fade-in");
     about.style.display = "block";
+    about.classList.add("fade-in");
     initMap();
 }
+
